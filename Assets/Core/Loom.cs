851e02d29c4648638b5242565741446b4967e6d0
Latif Khalifa <latifer@streamgrid.net>
2013-08-11 18:54:40 +0200

    Safe multithreading

---
+++ Assets/Core/Loom.cs
@@ -0,0 +1,146 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+using System.Linq;
+
+public class Loom : MonoBehaviour
+{
+	static Loom _current;
+    static int _mainThreadID;
+
+	public static Loom Current
+	{
+		get
+		{
+			Initialize();
+			return _current;
+		}
+	}
+	
+	void Awake()
+	{
+		_current = this;
+		initialized = true;
+	}
+	
+	static bool initialized;
+	
+	static void Initialize()
+	{
+		if (!initialized)
+		{
+		
+			if(!Application.isPlaying)
+				return;
+			initialized = true;
+			var g = new GameObject("Loom");
+			_current = g.AddComponent<Loom>();
+            _mainThreadID = Thread.CurrentThread.ManagedThreadId;
+		}
+			
+	}
+	
+	private List<Action> _actions = new List<Action>();
+	public struct DelayedQueueItem
+	{
+		public float time;
+		public Action action;
+	}
+	private List<DelayedQueueItem> _delayed = new  List<DelayedQueueItem>();
+
+	List<DelayedQueueItem> _currentDelayed = new List<DelayedQueueItem>();
+	
+	public static void QueueOnMainThread(Action action)
+	{
+		QueueOnMainThread( action, 0f);
+	}
+
+    public static void QueueOnMainThread(Action action, float time)
+	{
+		if(time != 0)
+		{
+			lock(Current._delayed)
+			{
+				Current._delayed.Add(new DelayedQueueItem { time = Time.time + time, action = action});
+			}
+		}
+		else
+		{
+            if (Thread.CurrentThread.ManagedThreadId == _mainThreadID)
+            {
+                RunAction(action);
+            }
+            else
+            {
+                lock (Current._actions)
+                {
+                    Current._actions.Add(action);
+                }
+            }
+		}
+	}
+	
+	public static void RunAsync(Action a)
+	{
+		Initialize();
+		OpenMetaverse.WorkPool.QueueUserWorkItem(RunAction, a);
+	}
+	
+	private static void RunAction(object action)
+	{
+		try
+		{
+			((Action)action)();
+		}
+		catch (Exception ex)
+		{
+            Debug.LogWarning("Thread task error: " + ex.ToString());
+		}
+	}
+	
+	
+	void OnDisable()
+	{
+		if (_current == this)
+		{
+			_current = null;
+		}
+	}
+	
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	List<Action> _currentActions = new List<Action>();
+	
+	// Update is called once per frame
+	void Update()
+	{
+		lock (_actions)
+		{
+			_currentActions.Clear();
+			_currentActions.AddRange(_actions);
+			_actions.Clear();
+		}
+		foreach(var a in _currentActions)
+		{
+			a();
+		}
+		lock(_delayed)
+		{
+			_currentDelayed.Clear();
+			_currentDelayed.AddRange(_delayed.Where(d=>d.time <= Time.time));
+			foreach(var item in _currentDelayed)
+				_delayed.Remove(item);
+		}
+		foreach(var delayed in _currentDelayed)
+		{
+			delayed.action();
+		}
+	}
+}
+
